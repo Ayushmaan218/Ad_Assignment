db0715feed10980f8d937544a48930c760298108
Ayushmaan Mohanty <121386512+Ayushmaan218@users.noreply.github.com>
2023-10-12 21:57:10 +0530

    Add files via upload

---
+++ Assignment_1/a12.java
@@ -0,0 +1,16 @@
+package Assignment_1;
+
+public class a12 {
+    static double power(int N, int P)
+    {
+        return Math.pow(N, P);
+    }
+
+    public static void main(String[] args)
+    {
+        int N = 2;
+        int P = 3;
+
+        System.out.println(power(N, P));
+    }
+}
---
+++ Assignment_1/a13.java
@@ -0,0 +1,20 @@
+package Assignment_1;
+
+public class a13 {
+    static int power(int N, int P)
+    {
+        if (P == 0)
+            return 1;
+        else
+            return N * power(N, P - 1);
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int N = 2;
+        int P = 3;
+
+        System.out.println(power(N, P));
+    }
+}
